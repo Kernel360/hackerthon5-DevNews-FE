cc29a341baed6f09fd19b07871f5bf89bc05f979
Yeonggeun Lee <yeonggeun.lee@slexn.com>
2025-05-07 14:56:54 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, Outlet } from "react-router-dom";
+
+
+const Layout = () => {
+  return (
+    <Container>
+      <Nav/>
+
+      <Outlet/>
+    </Container>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout/>}>
+        <Route index element={<Home/>}/>
+        <Route path="join" element={<Join/>}/>
+        <Route path="login" element={<Detail/>}/>
+      </Route>
+    </Routes>
   );
 }
 
 export default App;
+
+
+const Container = styled.main`
+position: relative;
+min-height: calc(100vh - 250px);
+overflow-x: hidden;
+display: block;
+top: 72px;
+padding: 0 calc(3.5vw + 5px);
+
+&:after {
+  // background: url("/images/home-background.png") center center / cover no-repeat fixed;
+  content: "";
+  position: absolute;
+  inset: 0;
+  opacity: 1;
+  z-index: -1;
+}
